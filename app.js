f5c5ff39575c2fe3653b2ac3abdb150a0b67105c
pajith551@gmail.com <pajith551@gmail.com>
2022-03-11 14:33:56 +0530

    app completed

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
-const https = require("https")
+const https = require("https");
+const bodyParser = require("body-parser");
+const req = require("express/lib/request");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=6d76d29d42d25f4b3d105ee2b00ef54e";
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+    // console.log(req.body.cityName);
+    // const query = "chennai";
+    const query = req.body.cityName;
+    const appKey = "6d76d29d42d25f4b3d105ee2b00ef54e";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appKey + "&units=" + unit;
 
     https.get(url, function(response){
         console.log(response.statusCode); 
@@ -13,15 +24,20 @@ app.get("/", function(req, res){
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
-            console.log(temp);
             const desc = weatherData.weather[0].description;
-            console.log(desc);
-
+            const icon = weatherData.weather[0].icon;
+            const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently "+ desc + " </p>")
+            res.write("<h1>The temperature in "+ query +" is " + temp + "</h1>")  
+            res.write("<img src=" + imgUrl + ">" );
+            // res.send()   
         });
     });
-    res.send("Server is running");
-});
-
+    
+})
 app.listen(3000, function(){
     console.log("server running");
 });
+
+
+
